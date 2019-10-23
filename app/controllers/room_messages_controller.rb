dcf0024d5c094dcb131f39faed5700476c5ec9ec
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-10-23 13:23:19 -0600

    Finish app

--- app/controllers/room_messages_controller.rb
+++ app/controllers/room_messages_controller.rb
@@ -1,2 +1,18 @@
+require 'pry'
 class RoomMessagesController < ApplicationController
+  before_action :load_entities
+
+  def create
+    @room_message = RoomMessage.create user_id: current_user.id,
+                                       room: @room,
+                                       message: params.dig(:room_message, :message)
+    RoomChannel.broadcast_to @room, @room_message
+    redirect_to "/rooms/#{@room.id}"
+  end
+
+  protected
+
+  def load_entities
+    @room = Room.find params.dig(:room_message, :room_id)
+  end
 end
--- app/models/room_message.rb
+++ app/models/room_message.rb
@@ -1,3 +1,9 @@
+require 'pry'
 class RoomMessage < ApplicationRecord
   belongs_to :room, inverse_of: :room_messages
+
+  def as_json(options)
+    #binding.pry
+    super(options).merge(user_avatar_url: User.find(user_id).gravatar_url)
+  end
 end
